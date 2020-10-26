d02afc69095f3b44d117b43f7bbc9997c729eb61
sara <haochi_no_douhua@cpe-172-101-51-232.maine.res.rr.com>
2020-10-26 15:05:35 +0900

    全機能実装できました

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
   def index
     @tweets = Tweet.all
@@ -10,10 +11,14 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
-    if @tweet.save
-      redirect_to tweets_path, notice: "ついーとしました"
-    else
+    if params[:back]
       render :new
+    else
+      if @tweet.save
+        redirect_to tweets_path, notice: "ついーとしました"
+      else
+        render :new
+      end
     end
   end
 
@@ -21,6 +26,21 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "けしました"
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+       redirect_to tweets_path, notice: "へんしゅうしました"
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
   end
 
   private
@@ -29,10 +49,7 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:content)
   end
 
-
-
-
-
-
-
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
